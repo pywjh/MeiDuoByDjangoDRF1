3ff1c6b54a3d8be4845d2b62b541f7ba19e5c912
chao <chao@itcast.cn>
2019-04-01 15:21:27 +0800

    短信验证码js和html代码补充

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -1,75 +1,129 @@
 var vm = new Vue({
-	el: '#app',
-	data: {
-		error_name: false,
-		error_password: false,
-		error_check_password: false,
-		error_phone: false,
-		error_allow: false,
-		error_sms_code: false,
-		sending_flag: false,
+    el: '#app',
+    data: {
+        error_name: false,
+        error_password: false,
+        error_check_password: false,
+        error_phone: false,
+        error_allow: false,
+        error_sms_code: false,
+        sending_flag: false,
 
-		username: '',
-		password: '',
-		password2: '',
-		mobile: '', 
-		sms_code: '',
-		allow: false
-	},
-	methods: {
-		check_username: function (){
-			var len = this.username.length;
-			if(len<5||len>20) {
-				this.error_name = true;
-			} else {
-				this.error_name = false;
-			}
-		},
-		check_pwd: function (){
-			var len = this.password.length;
-			if(len<8||len>20){
-				this.error_password = true;
-			} else {
-				this.error_password = false;
-			}		
-		},
-		check_cpwd: function (){
-			if(this.password!=this.password2) {
-				this.error_check_password = true;
-			} else {
-				this.error_check_password = false;
-			}		
-		},
-		check_phone: function (){
-			var re = /^1[345789]\d{9}$/;
-			if(re.test(this.mobile)) {
-				this.error_phone = false;
-			} else {
-				this.error_phone = true;
-			}
-		},
-		check_sms_code: function(){
-			if(!this.sms_code){
-				this.error_sms_code = true;
-			} else {
-				this.error_sms_code = false;
-			}
-		},
-		check_allow: function(){
-			if(!this.allow) {
-				this.error_allow = true;
-			} else {
-				this.error_allow = false;
-			}
-		},
-		// 注册
-		on_submit: function(){
-			this.check_username();
-			this.check_pwd();
-			this.check_cpwd();
-			this.check_phone();
-			this.check_sms_code();
-			this.check_allow();
-		}
-	}
+        username: '',
+        password: '',
+        password2: '',
+        mobile: '',
+        sms_code: '',
+        allow: false,
+        sms_code_tip: '获取短信验证码',  // a标签文字
+        error_sms_code_message: ''  // 验证码错误提示信息
+
+    },
+    methods: {
+        check_username: function () {
+            var len = this.username.length;
+            if (len < 5 || len > 20) {
+                this.error_name = true;
+            } else {
+                this.error_name = false;
+            }
+        },
+        check_pwd: function () {
+            var len = this.password.length;
+            if (len < 8 || len > 20) {
+                this.error_password = true;
+            } else {
+                this.error_password = false;
+            }
+        },
+        check_cpwd: function () {
+            if (this.password != this.password2) {
+                this.error_check_password = true;
+            } else {
+                this.error_check_password = false;
+            }
+        },
+        check_phone: function () {
+            var re = /^1[3456789]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_phone = false;
+            } else {
+                this.error_phone = true;
+            }
+        },
+        check_sms_code: function () {
+            if (!this.sms_code) {
+                this.error_sms_code = true;
+            } else {
+                this.error_sms_code = false;
+            }
+        },
+        check_allow: function () {
+            if (!this.allow) {
+                this.error_allow = true;
+            } else {
+                this.error_allow = false;
+            }
+        },
+
+        // 发送短信验证码
+        send_sms_code: function () {
+            if (this.sending_flag == true) {
+                return;
+            }
+            this.sending_flag = true;
+
+            // 校验参数，保证输入框有数据填写
+            this.check_phone();
+
+            if (this.error_phone == true) {
+                this.sending_flag = false;
+                return;
+            }
+
+            // 向后端接口发送请求，让后端发送短信验证码
+            axios.get('http://127.0.0.1:8000' + '/sms_codes/' + this.mobile + '/', {
+                responseType: 'json'
+            })
+                .then(response => {
+                    // 表示后端发送短信成功
+                    // 倒计时60秒，60秒后允许用户再次点击发送短信验证码的按钮
+                    var num = 60;
+                    // 设置一个计时器
+                    var t = setInterval(() => {
+                        if (num == 1) {
+                            // 如果计时器到最后, 清除计时器对象
+                            clearInterval(t);
+                            // 将点击获取验证码的按钮展示的文本回复成原始文本
+                            this.sms_code_tip = '获取短信验证码';
+                            // 将点击按钮的onclick事件函数恢复回去
+                            this.sending_flag = false;
+                        } else {
+                            num -= 1;
+                            // 展示倒计时信息
+                            this.sms_code_tip = num + '秒';
+                        }
+                    }, 1000, 60)
+                })
+                .catch(error => {
+                    if (error.response.status == 400) {
+                        // 展示发送短信错误提示
+                        this.error_sms_code = true;
+                        this.error_sms_code_message = error.response.data.message;
+                    } else {
+                        console.log(error.response.data);
+                    }
+                    this.sending_flag = false;
+                })
+        },
+        // 注册
+        on_submit: function () {
+            this.check_username();
+            this.check_pwd();
+            this.check_cpwd();
+            this.check_phone();
+            this.check_sms_code();
+            this.check_allow();
+        }
+    }
 });
