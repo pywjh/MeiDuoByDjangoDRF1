aa895f6547518786f62c4c5693af4a405dbe437f
chao <chao@itcast.cn>
2019-04-01 11:01:05 +0800

    自定义用户模型及追加导包路径

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    """自定义用户模型类"""
+    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
+
+    class Meta:  # 配置数据库表名,及设置模型在admin站点显示的中文名
+        db_table = 'tb_users'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
