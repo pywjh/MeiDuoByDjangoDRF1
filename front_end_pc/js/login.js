69be318d24a3ad73e78272eceebf586c3c083106
chao <chao@itcast.cn>
2019-04-02 16:47:19 +0800

    JWT登录响应问题代码分析

--- front_end_pc/js/login.js
+++ front_end_pc/js/login.js
@@ -46,7 +46,7 @@ var vm = new Vue({
                         password: this.password
                     }, {
                         responseType: 'json',
-                        withCredentials: true
+                        withCredentials: true  // 跨域情况可以携带cookie
                     })
                     .then(response => {
                         // 使用浏览器本地存储保存token
