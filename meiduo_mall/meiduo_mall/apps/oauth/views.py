e51ae5f55b7bed40c2f060c70ebf42ddaab219d3
chao <chao@itcast.cn>
2019-04-04 11:02:53 +0800

    QQ第三方登录之拼接QQ登录url

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from QQLoginTool.QQtool import OAuthQQ
+from rest_framework.response import Response
 
 # Create your views here.
+class QQOauthURLView(APIView):
+    """拼接好QQ登录网址"""
+
+    def get(self, request):
+        # 1.提取前端传入的next参数记录用户从那里去到login界面
+        next = request.query_params.get('next') or '/'
+
+
+        # 2.利用QQ登录SDK
+        oauth = OAuthQQ()
+        #  创建QQ登录工具对象
+        login_url = oauth.get_qq_url()
+        #  调用它里面的方法 拼接好QQ登录网址
+        return Response({'login_url': login_url})
