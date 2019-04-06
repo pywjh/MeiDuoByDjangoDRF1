a6af978a41ee6c22847ecbcbe073653d072ee56f
chao <chao@itcast.cn>
2019-04-06 16:04:07 +0800

    查询单一省或市接口实现

--- meiduo_mall/meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -1,1 +1,1 @@
-from django.conf.urls import urlfrom . import viewsurlpatterns = [    # 查询所有省    url(r'^areas/$', views.AreaListView.as_view()),]
+from django.conf.urls import urlfrom . import viewsurlpatterns = [    # 查询所有省    url(r'^areas/$', views.AreaListView.as_view()),    url(r'^areas/(?P<pk>\d+)/$', views.AreaDetailView.as_view()),]
--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Area
+from .serializers import AreaSerializer, SubsSerializer
 
 # Create your views here.
 class AreaListView(APIView):
@@ -11,7 +13,24 @@ class AreaListView(APIView):
     def get(self, request):
         # 1.获取指定的查询集
         qs = Area.objects.filter(parent=None)
+        # qs = Area.objects.all()
         # 2.创建序列化器进行序列化
-        serializer = ''
+        serializer = AreaSerializer(qs, many=True)
         # 3.响应
         return Response(serializer.data)
+
+
+class AreaDetailView(APIView):
+    """查询单一省或市"""
+
+    def get(self, request, pk):
+        # 1. 根据pk查询出指定的省或市
+        try:
+            area = Area.objects.get(id=pk)
+        except Area.DoesNotExist:
+            return Response({'message': '无效pk'}, status=status.HTTP_400_BAD_REQUEST)
+        # 2. 创建序列化器进行序列化
+        serializer = SubsSerializer(area)
+        # 3. 响应
+        return Response(serializer.data)
+
