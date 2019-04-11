3c299dd59c8ab4d5c406b08a234ebf634c13d103
chao <chao@itcast.cn>
2019-04-11 10:38:45 +0800

    商品列表界面视图定义及指定查询集

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from .models import SKU
 
 # Create your views here.
+
+class SKUListView(ListAPIView):
+    """商品列表数据查询"""
+
+    serializer_class = ''
+    # queryset = SKU.objects.filter()
+
+    def get_queryset(self):
+        """如果当前在视图中没有去定义get /post方法 那么就没法定义一个参数用来接收正则组提取出来的url路径参数, 可以利用视图对象的 args或kwargs属性去获取啊"""
+        category_id = self.kwargs.get('category_id')
+        return SKU.objects.filter(is_launched=True, category_id=category_id)
+
+    # def get(self, category_id):
