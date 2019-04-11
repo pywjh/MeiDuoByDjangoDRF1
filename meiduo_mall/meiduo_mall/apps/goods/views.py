98871e0ff851adc332ef0cfa79914302a381b3db
chao <chao@itcast.cn>
2019-04-11 11:31:31 +0800

    商品列表界面的分页和排序及测试

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -17,6 +17,7 @@ class SKUListView(ListAPIView):
     serializer_class = SKUSerializer
     # queryset = SKU.objects.filter()
 
+
     def get_queryset(self):
         """如果当前在视图中没有去定义get /post方法 那么就没法定义一个参数用来接收正则组提取出来的url路径参数, 可以利用视图对象的 args或kwargs属性去获取啊"""
         category_id = self.kwargs.get('category_id')
