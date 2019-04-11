a1277cd3086e91b8b74e04da1d63866f47b09752
chao <chao@itcast.cn>
2019-04-11 11:58:35 +0800

    商品详情界面静态化处理

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from . import models
-from celery_tasks.html.tasks import generate_static_list_search_html
+from celery_tasks.html.tasks import generate_static_list_search_html, generate_static_sku_detail_html
 
 
 class GoodsCategoryAdmin(admin.ModelAdmin):
@@ -27,6 +27,46 @@ class GoodsCategoryAdmin(admin.ModelAdmin):
         generate_static_list_search_html.delay()
 
 
+# @admin.register(models.SKU)
+class SKUAdmin(admin.ModelAdmin):
+    """商品模型站点管理类"""
+
+    def save_model(self, request, obj, form, change):
+
+        obj.save()  # 千万不要少了这一行,不然admin的保存就无效
+
+        generate_static_sku_detail_html.delay(obj.id)
+
+
+class SKUImageAdmin(admin.ModelAdmin):
+    """商品图片模型站点管理类"""
+
+    def save_model(self, request, obj, form, change):
+        """
+
+        :param request:
+        :param obj: 图片模型对象
+        :param form:
+        :param change:
+        :return:
+        """
+        obj.save()  # 千万不要少了这一行,不然admin的保存就无效
+
+        sku = obj.sku  # 通过外键获取图片模型对象所关联的sku模型的id
+        # 如果当前sku商品还没有默认图片,就给它设置一张默认图片
+        if not sku.default_image_url:
+            sku.default_image_url = obj
+
+            generate_static_sku_detail_html.delay(sku.id)
+
+    def delete_model(self, request, obj):
+        obj.delete()
+        sku = obj.sku  # 获取到图片模型对象关联的sku模型
+        generate_static_sku_detail_html.delay(sku.id)
+
+
+
+
 # Register your models here.
 admin.site.register(models.GoodsCategory, GoodsCategoryAdmin)
 admin.site.register(models.GoodsChannel)
@@ -34,6 +74,6 @@ admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
 admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU)
+admin.site.register(models.SKU, SKUAdmin)
 admin.site.register(models.SKUSpecification)
-admin.site.register(models.SKUImage)
+admin.site.register(models.SKUImage, SKUImageAdmin)
