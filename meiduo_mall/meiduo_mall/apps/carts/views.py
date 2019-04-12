88e2adf4ba0218bed646f2120ff610c957718ea5
chao <chao@itcast.cn>
2019-04-12 09:52:30 +0800

    购物车视图认证问题延后处理

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -6,8 +6,13 @@ from rest_framework.views import APIView
 class CartView(APIView):
     """购物车增删改查"""
 
+    def perform_authentication(self, request):
+        """重写此方法 直接pass 可以延后认证 延后到第一次通过 request.user 或request.auth才去做认证"""
+        pass
+
     def post(self, request):
         """新增"""
+        print('xxxx')
         pass
 
     def get(self, request):
