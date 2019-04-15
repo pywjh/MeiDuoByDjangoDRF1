55315d97e6efda70481718205165d9bcb8972ef4
chao <chao@itcast.cn>
2019-04-15 15:21:27 +0800

    支付后的验证及保存支付结束及修改订单状态

--- meiduo_mall/meiduo_mall/apps/payment/views.py
+++ meiduo_mall/meiduo_mall/apps/payment/views.py
@@ -8,9 +8,10 @@ from django.conf import settings
 import os
 
 from orders.models import OrderInfo
+from .models import Payment
+
 
 # Create your views here.
-
 class PaymentView(APIView):
     """生成支付链接"""
 
@@ -68,15 +69,35 @@ class PaymentStatusView(APIView):
         # 获取前端以查询字符串方式传入的数据
         queryDict = request.query_params
         # 将queryDict类型转换成字典(要将中间的sign 从里面移除,然后进行验证)
+        data = queryDict.dict()
         # 将sign这个数据从字典中移除
+        sign = data.pop('sign')
 
         # 创建alipay支付宝对象
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keys/app_private_key.pem'),  # 指定应用自己的私钥文件绝对路径
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keys/alipay_public_key.pem'), # 指定支付宝公钥文件的绝对路径
+            sign_type="RSA2",  # RSA 或者 RSA2  加密方式推荐使用RSA2
+            debug = settings.ALIPAY_DEBUG  # 默认False
+        )
 
         # 调用alipay SDK中  的verify方法进行验证支付结果是否支付宝回传回来的
-
-        # 取出美多商城订单编号  再取出支付宝交易号
-        # 把两个编号绑定到一起存储mysql
-        # 修改支付成功后的订单状态
+        success = alipay.verify(data, sign)
+        if success:
+            # 取出美多商城订单编号  再取出支付宝交易号
+            order_id = data.get('out_trade_no')  # 美多订单编号
+            trade_no = data.get('trade_no')  # 支付宝交易号
+            # 把两个编号绑定到一起存储mysql
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_no
+            )
+            # 修改支付成功后的订单状态
+            OrderInfo.objects.filter(order_id=order_id, status=OrderInfo.ORDER_STATUS_ENUM['UNPAID']).update(status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
+        else:
+            return Response({'message': '非法请求'}, status=status.HTTP_403_FORBIDDEN)
 
         # 把支付宝交易响应回给前端
-        pass
+        return Response({'trade_id': trade_no})
