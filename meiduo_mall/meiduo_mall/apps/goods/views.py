e73f0bb8a117e5e71c882036233dec6ecbd13e30
chao <chao@itcast.cn>
2019-04-15 17:18:02 +0800

    Elasticsearch使用

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from drf_haystack.viewsets import HaystackViewSet
 from rest_framework.generics import ListAPIView
 
 from .models import SKU
-from .serializers import SKUSerializer
+from .serializers import SKUSerializer, SKUSearchSerializer
 from rest_framework.filters import OrderingFilter
 
 
@@ -21,3 +22,12 @@ class SKUListView(ListAPIView):
         """如果当前在视图中没有去定义get /post方法 那么就没法定义一个参数用来接收正则组提取出来的url路径参数, 可以利用视图对象的 args或kwargs属性去获取啊"""
         category_id = self.kwargs.get('category_id')
         return SKU.objects.filter(is_launched=True, category_id=category_id)
+
+
+class SKUSearchViewSet(HaystackViewSet):
+    """
+    SKU搜索
+    """
+    index_models = [SKU]  # 指定查询集
+
+    serializer_class = SKUSearchSerializer  # 指定序列化器
